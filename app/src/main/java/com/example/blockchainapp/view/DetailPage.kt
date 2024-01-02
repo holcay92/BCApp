416e6b409ad2e869f5c0d72aefbeef24372d2c29
halilolcay <holcay@marun.edu.tr>
2024-01-02 13:32:31 +0300

    dial phone number added

--- app/src/main/java/com/example/blockchainapp/view/DetailPage.kt
+++ app/src/main/java/com/example/blockchainapp/view/DetailPage.kt
@@ -1,5 +1,8 @@
 package com.example.blockchainapp.view
 
+import android.content.Intent
+import android.net.Uri
+import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -10,14 +13,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -31,12 +40,15 @@ import com.example.blockchainapp.viewModel.ItemViewModel
 fun DetailPage(viewModel: ItemViewModel, argItemId: Int) {
     val item by viewModel.item.observeAsState()
     viewModel.getItemById(argItemId)
+    val ctx = LocalContext.current
+
+    var showDetails by remember { mutableStateOf(false) }
+
     Card(
         modifier = Modifier.padding(16.dp)
             .fillMaxWidth(),
         shape = RoundedCornerShape(8.dp),
         border = BorderStroke(2.dp, color = colorResource(id = R.color.BCYellow)),
-
     ) {
         Text(
             text = "İlan Detayları",
@@ -47,19 +59,55 @@ fun DetailPage(viewModel: ItemViewModel, argItemId: Int) {
             fontFamily = FontFamily.SansSerif,
         )
     }
+
     Column(
         modifier = Modifier
-            .fillMaxSize().padding(top = 80.dp)
+            .fillMaxSize()
+            .padding(top = 80.dp)
             .background(colorResource(id = R.color.BCYellow)),
     ) {
         if (item != null) {
             ItemTitle(title = "İlan Başlığı", value = item!!.itemTitle)
             ItemTitle(title = "İlan Fiyatı", value = item!!.itemPrice.toString())
             item!!.itemDescription?.let { ItemTitle(title = "İlan Açıklaması", value = it) }
-            ItemTitle(title = "İlan Sahibi", value = "${item!!.name} ${item!!.surname}")
-            // ItemTitle(title = "TC Kimlik No", value = item!!.ssn)
-            ItemTitle(title = "Telefon Numarası", value = item!!.phoneNumber)
+
+            // Check if details should be shown
+            if (showDetails) {
+                ItemTitle(title = "İlan Sahibi", value = "${item!!.name} ${item!!.surname}")
+                ItemTitle(title = "Telefon Numarası", value = item!!.phoneNumber)
+
+                // Show dial button
+                Button(
+                    onClick = {
+                        // dial the number
+                        val u = Uri.parse("tel:" + item!!.phoneNumber)
+
+                        val i = Intent(Intent.ACTION_DIAL, u)
+                        try {
+                            ctx.startActivity(i)
+                        } catch (s: SecurityException) {
+                            Toast.makeText(ctx, "An error occurred", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    },
+                    colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.light_theme)),
+                    modifier = Modifier.padding(top = 8.dp).align(Alignment.CenterHorizontally),
+                ) {
+                    Text("Ara")
+                }
+            } else {
+                // Show "Show" button
+                Button(
+                    onClick = { showDetails = true },
+                    modifier = Modifier.padding(top = 8.dp).align(Alignment.CenterHorizontally),
+                    colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.light_theme)),
+
+                ) {
+                    Text("İletişim Bilgilerini Göster")
+                }
+            }
         }
+
         Spacer(modifier = Modifier.width(10.dp))
     }
 }
